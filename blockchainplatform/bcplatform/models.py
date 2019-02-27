3e9213333e9c46850f70ea2fb49736513dadc7c4
markguindi1 <markguindi1@gmail.com>
2019-02-27 12:22:28 -0500

    Add Blockchain User model

--- blockchainplatform/bcplatform/models.py
+++ blockchainplatform/bcplatform/models.py
@@ -3,3 +3,14 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+
+class BlockchainUser(models.Model):
+    USER_TYPE_CHOICES = (
+        ('bcadmin', "Blockchain Admin"),
+        ('bcmember', "Blockchain Member"),
+        ('simpleuser', "Simple User"),
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_type = models.CharField(max_length=16, choices=USER_TYPE_CHOICES)
+
