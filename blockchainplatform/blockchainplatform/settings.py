0aacaea8f275a7a8f07f0f0ca536f9e2a019aebe
markguindi1 <markguindi1@gmail.com>
2019-02-27 11:58:23 -0500

    Fix links in base templates

--- blockchainplatform/blockchainplatform/settings.py
+++ blockchainplatform/blockchainplatform/settings.py
@@ -133,3 +133,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Login/Logout URLs
+
+LOGIN_URL = '/user/login'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
