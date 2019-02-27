cbbf7f2e68cdd2babd62542d89e623fafa93e419
markguindi1 <markguindi1@gmail.com>
2019-02-27 10:30:26 -0500

    Add User Accounts App from different project

--- blockchainplatform/blockchainplatform/settings.py
+++ blockchainplatform/blockchainplatform/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bcplatform.apps.BcplatformConfig',
+    'user_accounts.apps.UserAccountsConfig',
 ]
 
 MIDDLEWARE = [
--- blockchainplatform/user_accounts/models.py
+++ blockchainplatform/user_accounts/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
-
+from bcplatform.models import *
 
 # Create your models here.
