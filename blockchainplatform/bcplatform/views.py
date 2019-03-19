90818d04736b9dedfabb27b6c0b98750566e78c7
markguindi1 <markguindi1@gmail.com>
2019-03-19 18:43:28 -0400

    Continue implementing getting context data for Homepage

--- blockchainplatform/bcplatform/views.py
+++ blockchainplatform/bcplatform/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.views.generic import *
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import *
 
+def get_blockchain_user(user):
+    return BlockchainUser.objects.get(user=user)
 
 # Create your views here.
 class HomepageView(LoginRequiredMixin, TemplateView):
     template_name = "bcplatform/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        blockchain_user = get_blockchain_user(self.request.user)
+
+        own_blockchains = blockchain_user.blockchains.all()
+        other_blockchains = blockchain_user.other_blockchains.all()
+
+        print(context)
+        return context
