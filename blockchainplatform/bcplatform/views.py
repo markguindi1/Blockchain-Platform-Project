824a9d6185bb5d55baeb2c9fad0cc1698fafd2a1
markguindi1 <markguindi1@gmail.com>
2019-03-19 18:00:34 -0400

    Add LoginRequiredMixin to homepage

--- blockchainplatform/bcplatform/views.py
+++ blockchainplatform/bcplatform/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import *
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
-class HomepageView(TemplateView):
+class HomepageView(LoginRequiredMixin, TemplateView):
     template_name = "bcplatform/index.html"
