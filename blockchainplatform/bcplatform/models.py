c335432ab8002cba0d9c95ee67a8c7f2179537b3
markguindi1 <markguindi1@gmail.com>
2019-03-19 17:01:24 -0400

    Create Block and Blockchain models

--- blockchainplatform/bcplatform/models.py
+++ blockchainplatform/bcplatform/models.py
@@ -5,12 +5,30 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class BlockchainUser(models.Model):
-    USER_TYPE_CHOICES = (
-        ('bcadmin', "Blockchain Admin"),
-        ('bcmember', "Blockchain Member"),
-        ('simpleuser', "Simple User"),
-    )
-
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    user_type = models.CharField(max_length=16, choices=USER_TYPE_CHOICES)
+
+    def get_own_blockchains(self):
+        pass
+
+    def get_other_blockchains(self):
+        pass
+
+
+class Block(models.Model):
+    data = models.TextField(max_length=255)
+    index = models.IntegerField(blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True) # Auto-sets the field to the time this object was created
+    previous_hash = models.CharField(max_length=255)
+    nonce = models.CharField(max_length=255)
+    hash = models.CharField(max_length=255, blank=True)
+    chain = models.ForeignKey('Blockchain', on_delete=models.CASCADE)
+
+
+class Blockchain(models.Model):
+    name = models.CharField(max_length=255, blank=False)
+    admin = models.OneToOneField(BlockchainUser, on_delete=models.CASCADE, related_name='+')
+    members = models.ManyToManyField(BlockchainUser, related_name='+')
+
+
+
 
