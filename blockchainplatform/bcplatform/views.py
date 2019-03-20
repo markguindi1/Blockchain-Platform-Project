af0a546964296d91f685a0fe47b911d9fd86196d
markguindi1 <markguindi1@gmail.com>
2019-03-20 13:24:25 -0400

    Change getting blockchains for better readability

--- blockchainplatform/bcplatform/views.py
+++ blockchainplatform/bcplatform/views.py
@@ -3,9 +3,6 @@ from django.views.generic import *
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import *
 
-def get_blockchain_user(user):
-    return BlockchainUser.objects.get(user=user)
-
 # Create your views here.
 class HomepageView(LoginRequiredMixin, TemplateView):
     template_name = "bcplatform/index.html"
@@ -13,10 +10,11 @@ class HomepageView(LoginRequiredMixin, TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        blockchain_user = get_blockchain_user(self.request.user)
+        blockchainuser = self.request.user.blockchainuser
 
-        own_blockchains = blockchain_user.blockchains.all()
-        other_blockchains = blockchain_user.other_blockchains.all()
+        own_blockchains = blockchainuser.blockchains.all()
+        other_blockchains = blockchainuser.other_blockchains.all()
 
-        print(context)
+        context["own_blockchains"] = own_blockchains
+        context["other_blockchains"] = other_blockchains
         return context
