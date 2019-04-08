b4df63fbbeb9f7c16fcb4421cda8e7a14e63011b
markguindi1 <markguindi1@gmail.com>
2019-04-07 20:10:10 -0400

    Comment out useless forms code

--- blockchainplatform/bcplatform/forms.py
+++ blockchainplatform/bcplatform/forms.py
@@ -1,7 +1,12 @@
-from django.forms import ModelForm
+from django.forms import Form, ModelForm
 from . import models
 
-class BlockchainForm(ModelForm):
-    class Meta:
-        model = models.Blockchain
-        fields = ['name', 'members']
+# class BlockchainForm(ModelForm):
+#     class Meta:
+#         model = models.Blockchain
+#         fields = ['name', 'members']
+
+# class BlockCorruptionForm(Form):
+#     pass
+
+
