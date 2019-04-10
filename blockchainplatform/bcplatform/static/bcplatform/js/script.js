2cf19a51765b6058c423d9f751c5e96ed7ab5fdd
markguindi1 <markguindi1@gmail.com>
2019-04-10 14:21:39 -0400

    Create initial JS directory and files

---
+++ blockchainplatform/bcplatform/static/bcplatform/js/script.js
@@ -0,0 +1,21 @@
+/*
+
+script.js
+
+*/
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    //do work
+
+    // Get all cells with hashes
+    $()
+
+    // For each cell:
+
+        // If cell before it is same:
+            // Color both green
+        // Else:
+            //Color both red
+
+});
