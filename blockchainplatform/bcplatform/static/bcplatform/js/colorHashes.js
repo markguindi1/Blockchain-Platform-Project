218fa22868529d98a9ab2c046671d095e187ff86
markguindi1 <markguindi1@gmail.com>
2019-04-22 16:30:50 -0400

    Slight refactoring and removing extra CSS classes when necessary

--- blockchainplatform/bcplatform/static/bcplatform/js/colorHashes.js
+++ blockchainplatform/bcplatform/static/bcplatform/js/colorHashes.js
@@ -1,13 +1,4 @@
 
-// Adding the .format() function to String prototype
-String.prototype.format = function() {
-  a = this;
-  for (k in arguments) {
-    a = a.replace("{" + k + "}", arguments[k])
-  }
-  return a
-};
-
 /*
 This function colors the hashes and prev hashes of each block an appropriate color.
 If the prevHash of a block matches the hash of the previous block, then both
@@ -15,7 +6,7 @@ will appear green (by adding a Bootstrap class to the cell). If there's a
 mismatch, then both will appear red.
 */
 
-var colorAllHashes = function(event) {
+var colorAllHashes = function() {
     var blockchains = window.CONFIG.blockchains;
 
     // For each blockchain on the page:
@@ -29,15 +20,15 @@ var colorBlockchainHashes = function(bcId, numBlocks){
   // For each block on the blockchain:
   for (var i = 1; i < numBlocks; i++) {
       // Get Id's of each block's table
-      var blockTableId = "{0}-{1}-table".format(bcId, i);
-      var prevBlockTableId = "{0}-{1}-table".format(bcId, i-1);
+      var blockTableId = getBlockTableId(bcId, i);
+      var prevBlockTableId = getBlockTableId(bcId, i-1);
 
       // Get actual cells with the hash and previousHash we're comparing
-      var prevHashTd = $('#' + blockTableId).find("td.prev-hash");
-      var prevBlockHashTd = $('#' + prevBlockTableId).find("td.hash");
+      var $prevHashTd = $('#' + blockTableId).find("td.prev-hash");
+      var $prevBlockHashTd = $('#' + prevBlockTableId).find("td.hash");
 
       // Compare them, and change
-      if (prevHashTd.text() === prevBlockHashTd.text()){
+      if ($prevHashTd.text() === $prevBlockHashTd.text()){
           var classToAdd = "table-success";
           var classToRemove = "table-danger";
       }
@@ -46,12 +37,14 @@ var colorBlockchainHashes = function(bcId, numBlocks){
           var classToRemove = "table-success";
       }
 
-      prevHashTd.addClass(classToAdd);
-      prevHashTd.removeClass(classToRemove);
+      $prevHashTd.addClass(classToAdd);
+      $prevHashTd.removeClass(classToRemove);
 
-      prevBlockHashTd.addClass(classToAdd);
-      prevBlockHashTd.removeClass(classToRemove);
+      $prevBlockHashTd.addClass(classToAdd);
+      $prevBlockHashTd.removeClass(classToRemove);
   }
 }
 
-document.addEventListener("DOMContentLoaded", colorAllHashes);
+document.addEventListener("DOMContentLoaded", function(event){
+  colorAllHashes();
+});
