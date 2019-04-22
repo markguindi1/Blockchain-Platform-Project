c6a3153ad84febd5ef1b3a3ed184d07ffbbde12e
markguindi1 <markguindi1@gmail.com>
2019-04-21 21:08:54 -0400

    Change BlockCreateView to use Form and FormView instead of CreateView so Blockchain class has consistent API and methods for performing operations

--- blockchainplatform/bcplatform/views/blockchain_operation_views.py
+++ blockchainplatform/bcplatform/views/blockchain_operation_views.py
@@ -4,15 +4,17 @@ from django.urls import reverse, reverse_lazy
 from django.core.exceptions import PermissionDenied
 from ..models import *
 from .util_views import *
+from ..forms import BlockForm
 
 BLOCK_FORM_FIELDS = ['data']
 
 
-class BlockCreateView(OwnerOrMemberRequiredMixin, LoginRequiredMixin, CreateView):
+class BlockCreateView(OwnerOrMemberRequiredMixin, LoginRequiredMixin, FormView):
     """This view allows a user to add data to a blockchain. Handles both the GET and POST"""
-    model = Block
-    fields = BLOCK_FORM_FIELDS
+    # model = Block
+    # fields = BLOCK_FORM_FIELDS
     template_name = "bcplatform/block_create_form.html"
+    form_class = BlockForm
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
@@ -23,9 +25,8 @@ class BlockCreateView(OwnerOrMemberRequiredMixin, LoginRequiredMixin, CreateView
     def form_valid(self, form):
         chain_pk = self.kwargs['bc_pk']
         bc = Blockchain.objects.get(pk=chain_pk)
-        # Populates the newly created block object with data which the backend handles, such as the timestamp, hash,
-        # previous_hash, index, etc. form.instance is the newly create block.
-        bc.populate_block(form.instance)
+        data = form.cleaned_data['data']
+        bc.append_data(data)
         return super().form_valid(form)
 
     def get_success_url(self):
