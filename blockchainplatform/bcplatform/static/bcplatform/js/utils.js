40fa8f0134b3bc8fffd5a2f3f6112124981ae697
markguindi1 <markguindi1@gmail.com>
2019-04-22 16:31:12 -0400

    Add file for JS utility functions

---
+++ blockchainplatform/bcplatform/static/bcplatform/js/utils.js
@@ -0,0 +1,21 @@
+
+/*
+Utility functions
+*/
+
+// Adding the .format() function to String prototype
+String.prototype.format = function() {
+  a = this;
+  for (k in arguments) {
+    a = a.replace("{" + k + "}", arguments[k])
+  }
+  return a
+};
+
+var getBlockchainTableId = function(bcId){
+  return "{0}-table".format(bcId);
+}
+
+var getBlockTableId = function(bcId, blockI){
+  return "{0}-{1}-table".format(bcId, blockI);
+}
