bcde2c447735b83f92253dd4ed6e1b3a1f287b79
markguindi1 <markguindi1@gmail.com>
2019-04-22 13:01:17 -0400

    Refactor colorHashes into multiple functions

--- blockchainplatform/bcplatform/static/bcplatform/js/colorHashes.js
+++ blockchainplatform/bcplatform/static/bcplatform/js/colorHashes.js
@@ -6,7 +6,7 @@ String.prototype.format = function() {
     a = a.replace("{" + k + "}", arguments[k])
   }
   return a
-}
+};
 
 /*
 This function colors the hashes and prev hashes of each block an appropriate color.
@@ -14,28 +14,45 @@ If the prevHash of a block matches the hash of the previous block, then both
 will appear green (by adding a Bootstrap class to the cell). If there's a
 mismatch, then both will appear red.
 */
-document.addEventListener("DOMContentLoaded", function(event) {
-    var numBCs = window.CONFIG.bc_ids.length;
-    // For each cell:
 
+var colorAllHashes = function(event) {
+    var numBCs = window.CONFIG.bc_ids.length;
+
+    // For each blockchain on the page:
     for (var j = 0; j < numBCs; j++){
         var bcId = window.CONFIG.bc_ids[j];
         var numBlocks = window.CONFIG.bc_num_blocks[j];
-        for (var i = 1; i < numBlocks; i++) {
-            var blockTableId = "{0}-{1}-table".format(bcId, i);
-            var prevBlockTableId = "{0}-{1}-table".format(bcId, i-1);
-
-            var prevHashTd = $('#' + blockTableId).find("td.prev-hash");
-            var prevBlockHashTd = $('#' + prevBlockTableId).find("td.hash");
-
-            if (prevHashTd.text() === prevBlockHashTd.text()){
-                var classToAdd = "table-success";
-            }
-            else{
-                var classToAdd = "table-danger";
-            }
-            prevHashTd.addClass(classToAdd);
-            prevBlockHashTd.addClass(classToAdd);
-        }
+        colorBlockchainHashes(bcId, numBlocks);
     }
-});
+}
+
+var colorBlockchainHashes = function(bcId, numBlocks){
+  // For each block on the blockchain:
+  for (var i = 1; i < numBlocks; i++) {
+      // Get Id's of each block's table
+      var blockTableId = "{0}-{1}-table".format(bcId, i);
+      var prevBlockTableId = "{0}-{1}-table".format(bcId, i-1);
+
+      // Get actual cells with the hash and previousHash we're comparing
+      var prevHashTd = $('#' + blockTableId).find("td.prev-hash");
+      var prevBlockHashTd = $('#' + prevBlockTableId).find("td.hash");
+
+      // Compare them, and change
+      if (prevHashTd.text() === prevBlockHashTd.text()){
+          var classToAdd = "table-success";
+          var classToRemove = "table-danger";
+      }
+      else{
+          var classToAdd = "table-danger";
+          var classToRemove = "table-success";
+      }
+
+      prevHashTd.addClass(classToAdd);
+      prevHashTd.removeClass(classToRemove);
+
+      prevBlockHashTd.addClass(classToAdd);
+      prevBlockHashTd.removeClass(classToRemove);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", colorHashes);
